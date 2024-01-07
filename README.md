0f0e8073b065bac5d58cc9a0739bbb0f724d01d5
msfmydream <msfmydream@gmail.com>
2024-01-07 10:44:29 +0800

    spring project init

---
+++ README.md
@@ -0,0 +1,12 @@
+## SpringBoot 项目初始模板
+
+## 预设功能
+- Spring Boot (2.7.0)
+- Spring MVC
+- MySQL driver
+- Mybatis
+- Mybatis Plus
+- Spring AOP
+- Apache Commons Lang3
+- Lombok
+- swagger + knife4j 接口文档
---
+++ src/main/java/com/msfmydream/MyApplication.java
@@ -0,0 +1,14 @@
+package com.msfmydream;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.msfmydream.mapper")
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
